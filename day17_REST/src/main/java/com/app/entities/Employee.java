c58226e350af19e5b8a85696fd855214a4327f5c
RajkumarKarnakar <147190172+RajkumarKarnakar@users.noreply.github.com>
2024-01-19 02:02:21 +0530

    Add files via upload

---
+++ day17_REST/src/main/java/com/app/entities/Employee.java
@@ -0,0 +1,91 @@
+package com.app.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+//@Table(name = "emps") // to specify table name
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString(callSuper = true)
+
+public class Employee extends BaseEntity {
+	//@JsonProperty(value = "fn")
+	@Column( length = 20) // varchar(20)
+	private String firstName;//dervied col name : first_name
+	@Column( length = 20)
+	private String lastName;
+	@Column(length = 30, unique = true) // unique constraint
+	private String email;
+	@Column(length = 20, nullable = false) // NOT NULL constraint
+	@JsonProperty(access=Access.WRITE_ONLY)
+	private String password;	
+	private LocalDate joinDate;
+	private double salary;
+	@Column(length = 30)
+	private String location;
+	@Column(length = 30)
+	private String department;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public LocalDate getJoinDate() {
+		return joinDate;
+	}
+	public void setJoinDate(LocalDate joinDate) {
+		this.joinDate = joinDate;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}	
+}
